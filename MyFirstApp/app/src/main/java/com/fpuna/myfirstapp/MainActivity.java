2a8b0d8c38ec5b4627a5dd97022e015930922c1f
dianamelga <di.melgarejo@gmail.com>
2017-02-27 16:26:52 -0300

    Agrego una nueva activity de Login con la API de Google, que cuando inicia sesion se muestra un boton "GO" que nos lleva a nuestra antigua MainActivity

--- MyFirstApp/app/src/main/java/com/fpuna/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/com/fpuna/myfirstapp/MainActivity.java
@@ -21,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
+
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
+/*
+        Intent intent = new Intent(this, LoginActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        startActivity(intent);
+*/
     }
 
 }
